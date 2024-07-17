62e509ce15eab2789a05b1b69e768400b5f51403
tsu <smthanh@gmail.com>
2024-07-17 23:20:08 +0700

    Add support for blob storage from Vercel
    Add blob storage hostname to next.config.js
    Add .env*.local to gitignore
    Prevents accidental deployment of environment variables

--- next.config.js
+++ next.config.js
@@ -18,6 +18,7 @@ module.exports = {
       { hostname: "www.google.com" },
       { hostname: "flag.vercel.app" },
       { hostname: "illustrations.popsy.co" },
+      { hostname: "zkvfstxgznqvmwql.public.blob.vercel-storage.com" },
     ],
   },
 };
