76b11af5ab7d4b85783c14bf998bbcf30fc67eec
tsu <smthanh@gmail.com>
2024-07-19 00:42:14 +0700

    Add Umami analytics tracking
    Include Umami script in the layout
    Defer the script loading
    Provide the website ID
    Enable tracking on all pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Analytics } from "@vercel/analytics/react";
 import { Providers } from "./providers";
 import { Metadata } from "next";
 import { cn } from "@/lib/utils";
+import Script from "next/script";
 
 const title =
   "Platforms Starter Kit – The all-in-one starter kit for building multi-tenant applications.";
@@ -37,6 +38,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <Script defer src="https://umami.webpilot.cc/script.js" data-website-id="a78f5c0a-3513-49cc-af63-4c655f051733"></Script>
       <body className={cn(cal.variable, inter.variable)}>
         <Providers>
           {children}
